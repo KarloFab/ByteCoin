4c5e69405d48aaed38ac0fc7054a5de1de547ae9
KarloFab <fabijanickarlo1@gmail.com>
2021-11-29 12:59:46 +0100

    Added methods, delegate and datasource for UIPickerView. Added CoinManager.

---
+++ ByteCoin/CoinManager.swift
@@ -0,0 +1,19 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Karlo Fabijanić on 29.11.2021..
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io./v1/exchangerate/BTC"
+    let apiKey = "apiKey"
+    let currencies = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String){
+    }
+    
+}
--- ByteCoin/Controllers/ViewController.swift
+++ ByteCoin/Controllers/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    let coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
     }
 
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencies.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencies[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let currency = coinManager.currencies[row]
+        coinManager.getCoinPrice(for: currency)
+    }
 }
 
